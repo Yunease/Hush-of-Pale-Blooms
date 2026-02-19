92a815cda1ef8f1c46d71ec250ed4180302f68e6
yunease <2642076599@qq.com>
2026-02-19 12:16:21 +0800

    修复了日期显示错误的问题

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -3,14 +3,18 @@ export function formatDateToYYYYMMDD(date: Date): string {
 }
 
 export function formatDate(date: Date): string {
-	const year = date.getFullYear().toString();
-	const month = (date.getMonth() + 1).toString().padStart(2, "0");
 	const day = date.getDate().toString().padStart(2, "0");
-	return `${year}.${month}.${day}`;
+	return `${day}日`;
 }
 
 export function formatTime(date: Date): string {
 	const hours = date.getHours().toString().padStart(2, "0");
 	const minutes = date.getMinutes().toString().padStart(2, "0");
 	return `${hours}:${minutes}`;
+}
+
+export function formatYearMonth(date: Date): string {
+	const year = date.getFullYear().toString();
+	const month = (date.getMonth() + 1).toString().padStart(2, "0");
+	return `${year}.${month}`;
 }
